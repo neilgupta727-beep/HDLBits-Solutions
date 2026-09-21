d4d1a494c9864f77cb07e79ee4b71adf3a492389
Neil Gupta <neilgupta@Neils-MacBook-Air.local>
2026-09-22 02:51:26 +0530

    Solved: 7458 in 2-Verilog-Language/1-Basics

---
+++ 2-Verilog-Language/1-Basics/7458.v
@@ -0,0 +1,20 @@
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    wire a,b,c,d;
+   
+    assign a= (p1a&p1b&p1c);
+    assign b= (p1d&p1e&p1f);
+    assign c= (p2a&p2b);
+    assign d= (p2c&p2d);
+    assign p1y = a|b;
+    assign p2y =c|d;
+    
+    
+
+
+endmodule
+
+
